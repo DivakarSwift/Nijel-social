fa378e749b46938a3312a3ee6750aa511a4de458
Nijel Hunt <hunt.ni@husky.neu.edu>
2018-08-30 23:40:46 -0400

    Initial Commit

---
+++ TrueApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrueApp
+//
+//  Created by Nijel Hunt on 8/30/18.
+//  Copyright © 2018 Nijel Hunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
